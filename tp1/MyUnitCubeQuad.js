a48a04ca7158f13bf8466a04831b030944f68f36
Miguel Duarte <gamer-go@hotmail.com>
2018-02-23 10:40:41 +0000

    Solved 4.8

--- tp1/MyUnitCubeQuad.js
+++ tp1/MyUnitCubeQuad.js
@@ -14,8 +14,45 @@ class MyUnitCubeQuad extends CGFobject
 	};
 
 	display() {
+		//Left of cube
+		this.scene.pushMatrix();
+		this.scene.rotate(-Math.PI/2, 0, 1, 0);
 		this.scene.translate(0, 0, 0.5);
-		
 		this.quad.display();
+		this.scene.popMatrix();
+
+		//Front of cube
+		this.scene.pushMatrix();
+		this.scene.translate(0, 0, 0.5);		
+		this.quad.display();
+		this.scene.popMatrix();
+
+		//Right of cube
+		this.scene.pushMatrix();
+		this.scene.rotate(Math.PI/2, 0, 1, 0);
+		this.scene.translate(0, 0, 0.5);		
+		this.quad.display();
+		this.scene.popMatrix();
+		
+		//Back of cube
+		this.scene.pushMatrix();
+		this.scene.rotate(Math.PI, 0, 1, 0);
+		this.scene.translate(0, 0, 0.5);		
+		this.quad.display();
+		this.scene.popMatrix();
+
+		//Top of cube
+		this.scene.pushMatrix();
+		this.scene.rotate(-Math.PI/2, 1, 0, 0);
+		this.scene.translate(0, 0, 0.5);		
+		this.quad.display();
+		this.scene.popMatrix();
+
+		//Bottom of cube
+		this.scene.pushMatrix();
+		this.scene.rotate(Math.PI/2, 1, 0, 0);
+		this.scene.translate(0, 0, 0.5);		
+		this.quad.display();
+		this.scene.popMatrix();
 	};
 };
