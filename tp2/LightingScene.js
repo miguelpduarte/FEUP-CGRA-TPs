cf1fbeacf18f754706888a700cfc103611b6243c
Miguel Duarte <gamer-go@hotmail.com>
2018-03-02 17:56:12 +0000

    tp2 done except extra

--- tp2/LightingScene.js
+++ tp2/LightingScene.js
@@ -45,7 +45,7 @@ class LightingScene extends CGFscene
 		this.materialA.setDiffuse(0.6,0.6,0.6,1);
 		//this.materialA.setSpecular(0.2,0.2,0.2,1);
 		//this.materialA.setSpecular(0.8,0.8,0.8,1);	
-		this.materialA.setSpecular(0, 0, 0.8, 1);
+		this.materialA.setSpecular(0, 0.2, 0.8, 1);
 		//this.materialA.setShininess(10);
 		this.materialA.setShininess(120);
 
@@ -74,10 +74,11 @@ class LightingScene extends CGFscene
 		this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
 		this.lights[1].setVisible(true); // show marker on light position (different from enabled)
 
-		//this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
-		//this.lights[1].setVisible(true); // show marker on light position (different from enabled)
-		//this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
-		//this.lights[1].setVisible(true); // show marker on light position (different from enabled)
+		this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
+		this.lights[2].setVisible(true); // show marker on light position (different from enabled)
+
+		this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
+		this.lights[3].setVisible(true); // show marker on light position (different from enabled)
 
 		this.lights[0].setAmbient(0, 0, 0, 1);
 		this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
@@ -87,6 +88,22 @@ class LightingScene extends CGFscene
 		this.lights[1].setAmbient(0, 0, 0, 1);
 		this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
 		this.lights[1].enable();
+
+		this.lights[2].setAmbient(0, 0, 0, 1);
+		this.lights[2].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[2].setSpecular(1.0, 1.0, 1.0, 1.0);
+		this.lights[2].setConstantAttenuation(0.0);
+		this.lights[2].setLinearAttenuation(1.0);
+		this.lights[2].setQuadraticAttenuation(0.0);
+		this.lights[2].enable();
+
+		this.lights[3].setAmbient(0, 0, 0, 1);
+		this.lights[3].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[3].setSpecular(1.0, 1.0, 0.0, 1.0);
+		this.lights[3].setConstantAttenuation(0.0);
+		this.lights[3].setLinearAttenuation(0.0);
+		this.lights[3].setQuadraticAttenuation(0.2);
+		this.lights[3].enable();
 	};
 
 	updateLights() 
